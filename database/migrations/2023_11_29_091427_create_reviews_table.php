93366bd90b4f6e405f630494a2258e23364446f0
omar208 <55194832+omar208@users.noreply.github.com>
2023-11-30 15:26:26 +0200

    startadminpanel

---
+++ database/migrations/2023_11_29_091427_create_reviews_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('street_address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postal_code');
+            $table->string('country');
+            $table->decimal('latitude', 10, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->timestamps();
+
+            // Foreign key relationship
+            $table->foreign('user_id')->references('id')->on('users');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,24 @@
+<table>
+    <tr>
+        <th>
+            <img
+{{--             src="{{assest('images/name of image')}}"
+            where image folder in public   --}}
+            src="https://images.squarespace-cdn.com/content/v1/575a6067b654f9b902f452f4/1552683653140-0UUVQSSUEWVC73AWAEQG/300Logo.png",
+            alt="logo panel"
+            class="h-16">
+        </th>
+        <th>
+           <div
+            {{
+            $attributes->class([
+                'fi-logo text-xl font-bold leading-5 tracking-tight text-gray-950 dark:text-white',
+            ])
+             }}
+             >
+             Admin Panel
+         </div>
+        </th>
+      
+    </tr>
+</table>

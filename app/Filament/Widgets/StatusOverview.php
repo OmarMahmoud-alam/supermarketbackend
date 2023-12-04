038ef714493a977b401e89a3fb4df4e8b1fad0d9
omar208 <55194832+omar208@users.noreply.github.com>
2023-12-04 09:36:02 +0200

    done chart

--- app/Filament/Widgets/StatusOverview.php
+++ app/Filament/Widgets/StatusOverview.php
@@ -2,15 +2,41 @@
 
 namespace App\Filament\Widgets;
 
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use App\Models\Order;
+use App\Models\Custom;
+use App\Models\Product;
+use App\Enums\OrderStatusEnum;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class StatusOverview extends BaseWidget
-{
+{ 
+    protected static ?int $sort = 2;
+    protected static ?string $pollingInterval = '15s';
+
+    protected static bool $isLazy = true;
     protected function getStats(): array
     {
         return [
-            //
-        ];
+            Stat::make('Total Customers', Custom::count())
+            ->description('Increase in customers')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->color('success'),
+           // ->chart([7, 3, 4, 5, 6, 3, 5, 3]),
+          /*  Stat::make('verified custom', Custom::orWhereNull('email_verified_at' )->count())
+            ->description('Total verfied custom in app')
+            ->descriptionIcon('heroicon-m-arrow-trending-down')
+            ->color('danger')
+            ->chart([7, 3, 4, 5, 6, 3, 5, 3]), */
+        Stat::make('Total Products', Product::count())
+            ->description('Total products in app')
+            ->descriptionIcon('heroicon-m-arrow-trending-down')
+            ->color('danger')
+            ->chart([7, 3, 4, 5, 6, 3, 5, 3]),
+        Stat::make('Pending Orders', Order::where('status', OrderStatusEnum::PENDING->value)->count())
+            ->description('Total products in app')
+            ->descriptionIcon('heroicon-m-arrow-trending-down')
+            ->color('danger')
+            ->chart([7, 3, 4, 5, 6, 3, 5, 3]),        ];
     }
 }
